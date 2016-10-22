73d4e61f51788c148fb710721794ca6d2c5f36b8
wermington <wermington@gmail.com>
2016-10-22 18:52:07 +0200

    Added ModelView for projects
    * Model view for projects
    * Mapping for projects
    
    Signed-off-by: wermington <wermington@gmail.com>

--- Source/FaaS.MVC/Controllers/Web/ProjectsController.cs
+++ Source/FaaS.MVC/Controllers/Web/ProjectsController.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using FaaS.MVC.Models.Forms;
+using FaaS.MVC.Models.Projects;
+using FaaS.Services;
+using FaaS.Services.DataTransferModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FaaS.MVC.Controllers.Web
 {
     public class ProjectsController: Controller
     {
+        private readonly IFaaSService _faaSService;
+        private IMapper _mapper;
+        private static User _superUser = new User();
+
+        public ProjectsController(IFaaSService faaSService, IMapper mapper)
+        {
+            _faaSService = faaSService;
+            _mapper = mapper;
+        }
+
+        // GET: Products
+        [ActionName("Index")]
+        public async Task<IActionResult> List()
+        {
+            var projectsDTO = await _faaSService.GetAllProjects(_superUser);
+            return View(_mapper.Map<IEnumerable<ProjectModelView>>(projectsDTO));
+        }
     }
 }
