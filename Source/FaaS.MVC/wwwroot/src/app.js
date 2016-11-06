8806241fd53758ae108826946ee9f83dcad22699
wermington <wermington@gmail.com>
2016-11-06 12:07:43 +0100

    Login react and register
    Signed-off-by: wermington <wermington@gmail.com>

--- Source/FaaS.MVC/wwwroot/src/app.js
+++ Source/FaaS.MVC/wwwroot/src/app.js
@@ -1,10 +1,10 @@
 ﻿// src/index.js
 
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
-import Root from './Root';
+import "core-js/fn/object/assign";
+import React from "react";
+import ReactDOM from "react-dom";
+import { browserHistory, hashHistory } from "react-router";
+import Root from "./Root";
 
 // Render the main component into the dom
-ReactDOM.render(<Root history={browserHistory} />, document.getElementById('app'));
+ReactDOM.render(<Root history={hashHistory} />, document.getElementById('app'));
